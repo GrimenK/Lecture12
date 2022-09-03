834f0042433bfed39341662b87d48c6059ac0965
Oleh <oleh@Oleh.local>
2022-09-03 22:06:41 +0300

    4 tasks complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,76 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.jws.soap.SOAPBinding;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
 public class Main {
     public static final Logger LOGGER = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
 
+
+        firstTask();
+        fourthTask();
+        thirdTask();
     }
+
+    public static void firstTask(){
+        LOGGER.info("1st & 2nd TASK \n");
+        Class userClass = User.class;
+        Field[] fields = userClass.getDeclaredFields();
+        for (Field field:fields
+             ) {
+            if (field.isAnnotationPresent(CustomFieldAnnotation.class)){
+                LOGGER.info(field);
+                //task 2
+                LOGGER.info(field.getAnnotation(CustomFieldAnnotation.class).description());
+            }
+        }
+    }
+
+    public static void thirdTask(){
+        LOGGER.info("3rd TASK \n");
+        User user = new User();
+        user.setAge(4);
+
+        Class userClass = User.class;
+        try {
+            Method method = userClass.getDeclaredMethod("methodForInvoke", int.class);
+            method.setAccessible(true);
+            try {
+                LOGGER.info(method.invoke(user,200));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void fourthTask() throws IllegalAccessException, InstantiationException {
+        LOGGER.info("4th TASK \n");
+        User user = new User();
+
+        Class userClass = User.class;
+        Field[] fields = userClass.getDeclaredFields();
+        for (Field field:fields
+             ) {
+            if(!field.isAccessible()){
+                field.setAccessible(true);
+            }
+        }
+        fields[0].set(user,(String) "testName");
+        fields[1].set(user,(String) "FF123123");
+        fields[2].set(user,25);
+        fields[3].set(user,120000);
+        fields[4].set(user,(String) "tEST");
+        LOGGER.info(user);
+    }
+
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,72 @@
+public class User {
+
+    public String name;
+
+    @CustomFieldAnnotation(description = "This is field that contains persons id information")
+    private String documentID;
+
+    public int age;
+
+    @CustomFieldAnnotation(description = "This is field that contains persons income per year")
+    public int income;
+
+    public String lastName;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocumentID() {
+        return documentID;
+    }
+
+    public void setDocumentID(String documentID) {
+        this.documentID = documentID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", documentID='" + documentID + '\'' +
+                ", age=" + age +
+                ", income=" + income +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public int methodForInvoke(int age){
+        return this.age + age;
+    }
+}
